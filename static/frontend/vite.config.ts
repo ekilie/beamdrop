5a71cdd627069383dfff962771607069c3161a2a
tacheraSasi <tacherasasi@gmail.com>
2026-02-09 10:26:07 +0300

    refactor: remove server configuration from Vite config

--- static/frontend/vite.config.ts
+++ static/frontend/vite.config.ts
@@ -4,17 +4,6 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    allowedHosts: [
-      "371fc5bdf583.ngrok-free.app", // Add your ngrok domain here
-    ],
-
-    watch: {
-      usePolling: true, // Useful if using WSL, Docker, or ngrok
-    },
-  },
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean,
   ),

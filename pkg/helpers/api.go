602322b4b85f0d3850440ed756b932879bfa6068
tacheraSasi <tacherasasi@gmail.com>
2026-02-10 21:36:20 +0300

    move the helper functions to a separeate pacakge

---
+++ pkg/helpers/api.go
@@ -0,0 +1,23 @@
+package helpers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func SendJSON(w http.ResponseWriter, data any, status int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+}
+
+func SendAPIError(w http.ResponseWriter, code, message string, status int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(map[string]any{
+		"error": map[string]string{
+			"code":    code,
+			"message": message,
+		},
+	})
+}

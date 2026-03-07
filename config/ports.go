d2fd96f34dd2261622170568f6f22ffc7f2d082f
tacheraSasi <tacherasasi@gmail.com>
2026-03-08 01:42:10 +0300

    Format DefaultPorts for improved readability

--- config/ports.go
+++ config/ports.go
@@ -1,4 +1,15 @@
 package config
 
 // DefaultPorts is a list of ports to try if the primary port is unavailable
-var DefaultPorts = []int{7777, 8080, 8888, 9000, 9999, 3000, 4000, 5000, 6000, 8000}
+var DefaultPorts = []int{
+	7777, 
+	8080, 
+	8888, 
+	9000, 
+	9999, 
+	3000, 
+	4000, 
+	5000, 
+	6000, 
+	8000,
+}
